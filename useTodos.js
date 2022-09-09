05a817874d30584ac942a19dd05bcb3ce268f3d0
ivanbvn <96452713+Ivaan14@users.noreply.github.com>
2022-09-09 16:44:32 -0300

    Initial commit

---
+++ useTodos.js
@@ -0,0 +1,47 @@
+import { useEffect, useReducer } from "react"
+import {todoReducer} from "../todoReducer"
+
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || []
+}
+
+export const useTodos = (initialState = []) => {
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init)
+
+    useEffect(() => {
+      localStorage.setItem('todos',JSON.stringify(todos))
+    }, [todos])
+  
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+        dispatch(action)
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        })
+    }
+  
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+    }
+}
